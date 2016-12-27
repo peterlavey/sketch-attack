ff4c142eac4d10b588bdd17d8d3c355fb735681e
Pedro Cisternas <pedrocisternasd@gmail.com>
2016-12-27 17:12:19 -0300

    Render básico

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 let http = require('http');
+let open = require('open');
 let express = require('express');
 let app = express();
 
@@ -9,4 +10,5 @@ app.use(express.static(__dirname+'/public'));
 app.get('/', (req, res)=>res.sendFile('./public/index.html'));
 
 app.listen(PORT);
+open('http://localhost:3000/');
 console.log("Client running on port "+PORT);
